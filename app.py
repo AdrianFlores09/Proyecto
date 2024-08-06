aac684a5c6c6ac7db815dfd3e6ec9f061766293d
Adrian Flores <adrianflavad08@gmail.com>
2024-08-06 00:52:55 -0600

    Changes to be committed: 	modified:   app.py

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-        
+st.header("Odómetros de los vehículos en venta")        
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-        
-if hist_button: # al hacer clic en el botón
-           # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-            # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-        
-            # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
-    
-build_histogram = st.checkbox('Construir un histograma')
+build_histogram = st.checkbox('Construir un Histograma')
+build_dispersion = st.checkbox("Construir un Diagrama de Dispersión")
 
 if build_histogram: # si la casilla de verificación está seleccionada
     st.write('Construir un histograma para la columna odómetro')
+    px.title()
+    fig = px.histogram(car_data, x="odometer", title="Conteo de odómetros con su respectivo recorrido")
+    st.plotly_chart(fig, use_container_width=True)
+    
+if build_dispersion: # si la casilla de verificación está seleccionada
+    st.write('Construir un diagrama de dispersión para la columna odómetro')
+    fig = px.scatter(car_data, x="odometer", y="price", title="Precio de odómetros con su respectivo recorrido")
+    fig.show()
